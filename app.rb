45448016a2ff007ab391b82e0d5ba05d66ceb4c9
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-07 07:14:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/user_input' do
+    erb :user_input
+  end
+  
+  post '/user_input' do
+    user_text = params["text"]
+    piglatin = PigLatinizer.new(user_text)
+    @piglatined = piglatin.translate
+    erb :results
+  end
+  
 end
